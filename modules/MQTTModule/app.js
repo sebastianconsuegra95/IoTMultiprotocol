ac149c2bc90fa4ff5a2f3f573e38362eff618026
Sebastian Consuegra <sebastian951810@gmail.com>
2024-02-29 23:10:02 -0500

    Add MQTT Module and Env

--- modules/MQTTModule/app.js
+++ modules/MQTTModule/app.js
@@ -3,44 +3,55 @@
 var Transport = require('azure-iot-device-mqtt').Mqtt;
 var Client = require('azure-iot-device').ModuleClient;
 var Message = require('azure-iot-device').Message;
-var mqtt = require('mqtt')
+var Mqtt = require('mqtt');
 
-Client.fromEnvironment(Transport, function (err, client) {
-  if (err) {
-    throw err;
-  } else {
-    client.on('error', function (err) {
-      throw err;
-    });
+var ClientMqtt = Mqtt.connect('http://' + process.env.brokerIpAdress + ':1881');
+var Topics = ['prueba']
+var MQTT_Topics = Topics
 
-    // connect to the Edge instance
-    client.open(function (err) {
-      if (err) {
-        throw err;
-      } else {
-        console.log('IoT Hub module client initialized');
-
-        // Act on input messages to the module.
-        client.on('inputMessage', function (inputName, msg) {
-          pipeMessage(client, inputName, msg);
-        });
-      }
-    });
+ClientMqtt.on('connect', function () {
+  console.log('MQTT Connected!')
+  for (let i = 0; i < MQTT_Topics.length; i++) {
+    ClientMqtt.subscribe(MQTT_Topics[i])
   }
-});
+  console.log('Subscribe to all Topics!')
+})
+
+ClientMqtt.on('message', (topic, payload) => {
+  Client.fromEnvironment(Transport, function (err, client) {
+    if (err) {
+      throw err;
+    } else {
+      client.on('error', function (err) {
+        throw err;
+      });
+
+      // connect to the Edge instance
+      client.open(function (err) {
+        if (err) {
+          throw err;
+        } else {
+          var message = payload.toString('utf8')
+          var outputMsg = new Message(message)
+          client.sendOutputEvent('output1', outputMsg, printResultFor('Sending received message to output1'))
+        }
+      });
+    }
+  });
+})
 
 // This function just pipes the messages without any change.
-function pipeMessage(client, inputName, msg) {
-  client.complete(msg, printResultFor('Receiving message'));
+// function pipeMessage(client, inputName, msg) {
+//   client.complete(msg, printResultFor('Receiving message'));
 
-  if (inputName === 'input1') {
-    var message = msg.getBytes().toString('utf8');
-    if (message) {
-      var outputMsg = new Message(message);
-      client.sendOutputEvent('output1', outputMsg, printResultFor('Sending received message'));
-    }
-  }
-}
+//   if (inputName === 'input1') {
+//     var message = msg.getBytes().toString('utf8');
+//     if (message) {
+//       var outputMsg = new Message(message);
+//       client.sendOutputEvent('output1', outputMsg, printResultFor('Sending received message'));
+//     }
+//   }
+// }
 
 // Helper function to print results in the console
 function printResultFor(op) {
