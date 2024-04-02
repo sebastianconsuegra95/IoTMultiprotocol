dccf720edb35c0c67298bd4f4dd6cc7de51a44e8
lypachecoro <lpachecr@emeal.nttdata.com>
2024-04-01 21:36:29 -0500

    add modbusFormat

--- edgeSolution/modules/StandardModule/formats/modbusFormat.py
+++ edgeSolution/modules/StandardModule/formats/modbusFormat.py
@@ -1,5 +1,18 @@
 import json
 
 def format(input):
+    # Convert in python object (dic)
+    input_dict_json = json.loads(input.data)
 
-    return input
+    # Create diccionary with the desidered JSON structure
+    output_dict_json={
+        "device": input_dict_json["Content"][0]["HwId"],
+        "temperatura": input_dict_json["Content"][0]["Data"][0]["Values"][0]["Value"],
+        "presion": input_dict_json["Content"][0]["Data"][0]["Values"][1]["Value"]
+    }
+
+    # Convert in JSON
+    # ident -> identation level
+    json_output = json.dumps(output_dict_json, indent=2)
+
+    return json_output
